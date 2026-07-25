65a9614808a331d33fcb3eaf73d1a45daf202511
GitHub Action <action@github.com>
2026-07-25 13:34:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/glinte/refactorcli/MyBundle.kt
+++ src/main/kotlin/com/github/glinte/refactorcli/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.glinte.refactorcli
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/glinte/refactorcli/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/glinte/refactorcli/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.glinte.refactorcli.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
